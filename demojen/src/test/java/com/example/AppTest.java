2d04a27698a77f87b25bdbe5a2b99555e80de2db
neojarvis <techadmin@iamneo.ai>
2025-12-23 07:27:55 +0000

    Auto-save-commit-12/23/2025--12:57:52-PM

---
+++ demojen/src/test/java/com/example/AppTest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("--------------------SAMPLE TESTING DEMO WITH JENKINS-----------------------");
+    }
+}
